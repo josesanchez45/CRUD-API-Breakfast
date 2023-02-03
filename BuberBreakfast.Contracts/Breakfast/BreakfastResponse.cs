fc604025d68ea5e47d36465a981856ae54985bc6
jose sanchez <jose.sanchez.artist@gmail.com>
2023-02-02 23:12:52 -0800

    Starting commit

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,11 @@
+public record CreateBreakfastResponse(
+    Guid Id,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDateTime,
+    List<string> Savory,
+    List<string> Sweet
+
+);
